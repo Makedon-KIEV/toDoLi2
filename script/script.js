fd99feb345af25a977c6168606195f5998914a9a
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2022-01-19 14:53:52 +0200

    js fix
    done 1

--- script/script.js
+++ script/script.js
@@ -1,31 +1,33 @@
 let addMessage = document.querySelector('.message'),
-  addButton = document.querySelector('.add'),
-  todo = document.querySelector('.todo');
+    addButton = document.querySelector('.add'),
+    todo = document.querySelector('.todo');
 
 let todoList = [];
 
-addButton.addEventListener('click', function () {
+addButton.addEventListener('click', function() {
 
-  let newTodo = {
-    todo: addMessage.value,
-    checked: false,
-    important: false
-  };
+    let newTodo = {
+        todo: addMessage.value,
+        checked: true,
+        important: false
+    };
 
-  todoList.push(newTodo);
-  displayMessage();
+    todoList.push(newTodo);
+    displayMessage();
 
 });
 
 function displayMessage() {
-
-  todoList.forEach(function (item, i) {
-    let displayMessage = `
+    let displayMessage = '';
+    todoList.forEach(function(item, i) {
+        displayMessage += `
     <li>
-    <input type='checkbox' id=
+    <input type='checkbox' id='item_${i}' ${item.checked ? 'checked':''}>
+    <label for='item_${i}'>${item.todo}</label>
     </li>
- `;
-  })
+    `;
+        todo.innerHTML = displayMessage;
+    });
 };
 
 // let addMessage = document.querySelector('.message');
