f9c0b6eecd25aff24909df8c53357403909c30d9
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2022-01-19 21:37:13 +0200

    js
    js done

--- script/script.js
+++ script/script.js
@@ -29,19 +29,43 @@ function displayMessage() {
         displayMessage += `
     <li>
     <input type='checkbox' id='item_${i}' ${item.checked ? 'checked':'000'}>
-    <label for='item_${i}'>${item.todo}</label>
+    <label for='item_${i}' class="${item.important ? 'important' : ''}">${item.todo}</label>
     </li>
     `;
         todo.innerHTML = displayMessage;
     });
-};
+}
 
-// let addMessage = document.querySelector('.message');
-// let addButton = document.querySelector('.add');
-// let removeButton = document.querySelector('.remove');
-// let doneButton = document.querySelector('.done');
-// let clearButton = document.querySelector('.clear');
-// let todo = document.querySelector('.todo');
+todo.addEventListener('change', function(event) {
+
+    let forLabel = todo.querySelector('[for=' + event.target.getAttribute('id') + ']');
+    let valueLabel = forLabel.innerHTML;
+
+    todoList.forEach(function(item) {
+        if (item.todo === valueLabel) {
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
+});
+
+todo.addEventListener('contextmenu', function(event) {
+        event.preventDefault();
+        todoList.forEach(function(item) {
+            if (item.todo === event.target.innerHTML) {
+                item.important = !item.important;
+                displayMessages();
+                localStorage.setItem('todo', JSON.stringify(todoList));
+            }
+        })
+
+    })
+    // let addMessage = document.querySelector('.message');
+    // let addButton = document.querySelector('.add');
+    // let removeButton = document.querySelector('.remove');
+    // let doneButton = document.querySelector('.done');
+    // let clearButton = document.querySelector('.clear');
+    // let todo = document.querySelector('.todo');
 
 // let todoList = [];
 
