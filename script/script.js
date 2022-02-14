c682d329af59463451b844c5e4ec29b538b7e20e
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2022-02-14 09:55:35 +0200

    1
    1

--- script/script.js
+++ script/script.js
@@ -74,6 +74,12 @@ removeButton.addEventListener('click', function() {
     addMessage.value = '';
 });
 
+var array = []
+var checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
+
+for (var i = 0; i < checkboxes.length; i++) {
+    array.push(checkboxes[i].value)
+}
 // Button delete
 // removeButton.addEventListener('click', function() {
 
